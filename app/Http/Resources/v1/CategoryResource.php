b14476a6d1d9ed65f4bdd2086518615908f3d813
Gia Huy Tran <tonytran0314@gmail.com>
2024-07-30 00:13:32 -0400

    feat: add id to category resource

--- app/Http/Resources/v1/CategoryResource.php
+++ app/Http/Resources/v1/CategoryResource.php
@@ -16,6 +16,7 @@ class CategoryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'date' => dateFormatter($this->created_at),
